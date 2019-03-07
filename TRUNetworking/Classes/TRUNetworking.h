bb62eb86178a8b8b2750df08df5f9a0bab4deb71
Name <you@yourdomain.com>
2019-03-07 17:20:08 +0800

    init project

---
+++ TRUNetworking/Classes/TRUNetworking.h
@@ -0,0 +1,16 @@
+//
+//  TRUNetworking.h
+//  FBSnapshotTestCase
+//
+//  Created by 陈义蒙 on 2019/3/7.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TRUNetworking : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRUNetworking/Classes/TRUNetworking.m
@@ -0,0 +1,12 @@
+//
+//  TRUNetworking.m
+//  FBSnapshotTestCase
+//
+//  Created by 陈义蒙 on 2019/3/7.
+//
+
+#import "TRUNetworking.h"
+
+@implementation TRUNetworking
+
+@end
